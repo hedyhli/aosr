989dc407199b733a72aae34cc668e7625210230c
hedy <hedy@tilde.cafe>
2023-07-30 14:51:22 +0800

    add shuffle

--- arrangement.ts
+++ arrangement.ts
@@ -130,7 +130,7 @@ export class Arrangement extends ArrangementBase {
         if (this.allPattern.length > 0) {
             retlist.push(new ArrangementItem(TAGNAME.ALLTAG, this.allPattern.length, i18next.t('StartTextALL')))
         }
-        return retlist
+        return shuffle<ArrangementItem>(retlist)
     }
     private sort() {
         let now = window.moment()
@@ -202,3 +202,21 @@ export class Arrangement extends ArrangementBase {
         return true;
     }
 }
+
+export function shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+};
